864251f7d119a5b66ba986fcd29124e81e1f7965
Hernan Casé <hercase92@gmail.com>
2021-03-17 20:10:05 -0300

    add Regex to input

--- src/components/ChangeName/index.js
+++ src/components/ChangeName/index.js
@@ -1,25 +1,35 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { changeName } from "../../redux/actions";
 import "./index.scss";
 
 export const ChangeName = () => {
+  const [inputValue, setInputValue] = useState("");
   const dispatch = useDispatch();
   const input = useRef(null);
 
-  const handleChange = () => {
-    const newName = input.current.value;
-    if (newName.length > 3) dispatch(changeName(newName));
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (inputValue.length > 3) dispatch(changeName(inputValue));
+    setInputValue("");
   };
 
   return (
-    <div className="change-name">
-      <input ref={input} type="text" name="Nombre" required />
-      <button onClick={handleChange}>
+    <form className="change-name" onSubmit={handleChange}>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        ref={input}
+        type="text"
+        name="Nombre"
+        pattern="^[ñA-Za-z _]*[ñA-Za-z][ñA-Za-z _]*$"
+        required
+      />
+      <button>
         <span role="img" aria-label="change name">
           👌
         </span>
       </button>
-    </div>
+    </form>
   );
 };
